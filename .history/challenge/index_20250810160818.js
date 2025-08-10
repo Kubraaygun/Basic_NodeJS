046d9c1fec6a90227d24b93e31f9e7f41e025726
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-08-10 16:08:37 +0300

    update

--- .history/challenge/index_20250810160818.js
+++ .history/challenge/index_20250810160818.js
@@ -1,9 +1,9 @@
 // filesystem modulunu projeye dahil ediyoruz
 const fs = require("fs");
 // fs.readFileSync ile title.txtnin icerigini okuduk ve bu icerigi title(baslik) dfeğişkenine atadık
-const title = fs.readFileSync("./challenge/title.txt", "utf-8");
+const title = fs.readFileSync("./title.txt", "utf-8");
 // fs.readFileSync ile content.tsx icerigini okuduk ve bu icerigi title(baslik) dfeğişkenine atadık
-const content = fs.readFileSync("./challenge/content.txt", "utf-8");
+const content = fs.readFileSync("./content.txt", "utf-8");
 
 fs.writeFileSync(`./${title}`, content);
 console.log(`${title} title ismindeki dosya basariyla yazildi`);
